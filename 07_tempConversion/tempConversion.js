bac3acf8f18115439c729502280458bf58aea613
dibunbuns <bunniequeenrules@gmail.com>
2023-10-30 17:34:33 -0400

    finished though 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  let fixed = celsius.toFixed(1);
+  let roundedCelsius = parseFloat(fixed);
+  return roundedCelsius;
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = temp * (9 / 5) + 32;
+  let fixed = fahrenheit.toFixed(1);
+  let roundedFahrenheit = parseFloat(fixed);
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
